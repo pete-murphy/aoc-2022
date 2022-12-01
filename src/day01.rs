4b9f95e5a61610949bf6d456811a74c23641f01b
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-12-01 17:32:34 -0500

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,4 @@
+use std::collections::BinaryHeap;
 use std::env;
 use std::fs;
 
@@ -30,10 +31,29 @@ fn part1(input: &str) -> i64 {
     return max;
 }
 
+fn part2(input: &str) -> i64 {
+    let groups = input.split("\n\n");
+    let groups_split = groups.map(|group| group.split("\n"));
+    let summed = groups_split.map(|lines| {
+        lines
+            .map(|line| line.parse::<i64>().unwrap_or(0))
+            .sum::<i64>()
+    });
+
+    let heap: BinaryHeap<_> = summed.collect();
+
+    let maxes = heap.into_iter_sorted().take(3).sum::<i64>();
+
+    return maxes;
+}
+
 pub fn run() {
     let cwd = env::current_dir().unwrap();
     let input = fs::read_to_string(cwd.join("src").join("day01-input"));
     let input_ = input.expect("Failed to read input");
 
-    print!("{}", part1(input_.as_str()))
+    println!("part 1");
+    println!("{}", part1(input_.as_str()));
+    println!("part 2");
+    println!("{}", part2(input_.as_str()));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#![feature(binary_heap_into_iter_sorted)]
+
+use std::io::{self, Write};
 mod day01;
 
 fn main() {
-    day01::run()
+    day01::run();
+    io::stdout().flush();
 }
