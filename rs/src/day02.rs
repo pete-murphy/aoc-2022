77d15b64a6be347675df82f92c4a2f62f0cfe023
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-12-02 19:39:39 -0500

    Day 2, part 2

--- rs/src/day02.rs
+++ rs/src/day02.rs
@@ -5,14 +5,14 @@ B X
 C Z
 "#;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum RPS {
     Rock,
     Paper,
     Scissors,
 }
 
-fn parse_line(line: &str) -> Option<(RPS, RPS)> {
+fn parse_line_part1(line: &str) -> Option<(RPS, RPS)> {
     let split: Vec<_> = line.split(" ").collect();
     match split[..] {
         [x, y] => {
@@ -35,15 +35,35 @@ fn parse_line(line: &str) -> Option<(RPS, RPS)> {
     }
 }
 
-fn unformat(input: &str) -> impl Iterator<Item = (RPS, RPS)> + '_ {
-    return input.split("\n").filter_map(parse_line);
+#[derive(PartialEq, Eq)]
+enum WDL {
+    Win,
+    Draw,
+    Loss,
 }
 
-// enum Score {
-//     Win,
-//     Draw,
-//     Loss,
-// }
+fn parse_line_part2(line: &str) -> Option<(RPS, WDL)> {
+    let split: Vec<_> = line.split(" ").collect();
+    match split[..] {
+        [x, y] => {
+            let x_rps = match x {
+                "A" => RPS::Rock,
+                "B" => RPS::Paper,
+                "C" => RPS::Scissors,
+                _ => panic!("Not A, B, or C"),
+            };
+            let y_wdl = match y {
+                "X" => WDL::Loss,
+                "Y" => WDL::Draw,
+                "Z" => WDL::Win,
+                _ => panic!("Not X, Y, or Z"),
+            };
+            return Some((x_rps, y_wdl));
+        }
+
+        _ => return None,
+    }
+}
 
 fn score(round: (RPS, RPS)) -> i32 {
     let score_round = match round {
@@ -67,11 +87,36 @@ fn score(round: (RPS, RPS)) -> i32 {
 
     score_hand + score_round
 }
-
 fn part1(input: &str) -> i32 {
+    fn unformat(input: &str) -> impl Iterator<Item = (RPS, RPS)> + '_ {
+        return input.split("\n").filter_map(parse_line_part1);
+    }
+
     let parsed = unformat(input);
 
     parsed.map(score).sum()
+}
+
+fn part2(input: &str) -> i32 {
+    fn unformat(input: &str) -> impl Iterator<Item = (RPS, WDL)> + '_ {
+        return input.split("\n").filter_map(parse_line_part2);
+    }
+
+    fn get_rps(round: (RPS, WDL)) -> RPS {
+        match round {
+            (RPS::Rock, WDL::Win) => RPS::Paper,
+            (RPS::Paper, WDL::Win) => RPS::Scissors,
+            (RPS::Scissors, WDL::Win) => RPS::Rock,
+            (x, WDL::Draw) => x,
+            (RPS::Paper, WDL::Loss) => RPS::Rock,
+            (RPS::Scissors, WDL::Loss) => RPS::Paper,
+            (RPS::Rock, WDL::Loss) => RPS::Scissors,
+        }
+    }
+
+    let parsed = unformat(input);
+
+    parsed.map(|(x, y)| score((x, get_rps((x, y))))).sum()
 }
 
 pub fn run() {
@@ -82,4 +127,6 @@ pub fn run() {
 
     println!("part 1");
     println!("{}", part1(input_str));
+    println!("part 2");
+    println!("{}", part2(input_str));
 }
