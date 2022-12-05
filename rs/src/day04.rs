563fc33ad4b2c4534d298ac5a9a1605bb5d024b6
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-12-04 19:33:18 -0500

    Naive parser
    Works on sample input, but throws
    ```
    thread 'main' panicked at 'Oops', src/day04.rs:18:10
    ```

---
+++ rs/src/day04.rs
@@ -0,0 +1,61 @@
+use std::{env, fs};
+
+type Section = (u32, u32);
+
+fn parse_line_part1(line: &str) -> (Section, Section) {
+    let mut split = line.split(",").filter(|&l| !l.is_empty());
+    let x = split
+        .next()
+        .map(|x| {
+            println!("{}", x);
+            let mut sp = x
+                .split("-")
+                .map(|n| n.parse::<u32>().expect("Failed to parse int"));
+            let x1 = sp.next().expect("Oops y1");
+            let x2 = sp.next().expect("Oops y2");
+            (x1, x2)
+        })
+        .expect("Oops");
+    let y = split
+        .next()
+        .map(|x| {
+            let mut sp = x
+                .split("-")
+                .map(|n| n.parse::<u32>().expect("Failed to parse int"));
+            let y1 = sp.next().expect("Oops y1");
+            let y2 = sp.next().expect("Oops y2");
+            (y1, y2)
+        })
+        .expect("Oops");
+    (x, y)
+}
+
+fn sections_overlap(sections: (Section, Section)) -> bool {
+    (sections.0 .0 >= sections.1 .0 && sections.0 .1 <= sections.1 .1)
+        || (sections.1 .0 >= sections.0 .0 && sections.1 .1 <= sections.0 .1)
+}
+
+fn part1(input: &str) -> u32 {
+    let parsed = input.split("\n").map(parse_line_part1);
+
+    let mut n = 0;
+
+    for sections in parsed {
+        if sections_overlap(sections) {
+            n += 1;
+        }
+    }
+
+    n
+}
+
+pub fn run() {
+    let cwd = env::current_dir().unwrap();
+    let input = fs::read_to_string(cwd.join("../input/day04")).expect("Failed to read input");
+    let input_str = input.as_str();
+
+    println!("part 1");
+    println!("{}", part1(input_str));
+    // println!("part 2");
+    // println!("{}", part2(input_str));
+}
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -3,9 +3,11 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     day01::run();
     day02::run();
     day03::run();
+    day04::run();
 }
