fa29c5939aa14a687b1f9055c609531aa13c9881
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-12-05 20:11:48 -0500

    Day 5, part 2 (Rust)

--- rs/src/day05.rs
+++ rs/src/day05.rs
@@ -100,6 +100,72 @@ fn part1(input: &str) -> String {
     str_vec.into_iter().collect()
 }
 
+fn part2(input: &str) -> String {
+    let (&crates_lines, &instrs_lines) = match input.split("\n\n").collect::<Vec<_>>().as_slice() {
+        [crates, instrs] => (crates, instrs),
+        _ => panic!("Failed splitting into crates & instructions sections"),
+    };
+
+    let parsed_crates = crates_lines
+        .split("\n")
+        .take_while(|&x| !x.starts_with(" 1 "))
+        .map(parse_crate_line);
+
+    let parsed_crates_vec = parsed_crates.collect::<Vec<_>>();
+
+    // Using BTreeMap just so keys are sorted when we get values out
+    let mut m: BTreeMap<usize, Vec<char>> = BTreeMap::new();
+
+    for xs in parsed_crates_vec.iter().rev() {
+        for (j, x) in xs.iter().enumerate() {
+            match x {
+                &Some(c) => {
+                    m.entry(j + 1)
+                        .and_modify(|v| v.push(c))
+                        .or_insert([c].to_vec());
+                }
+                _ => (),
+            }
+        }
+    }
+
+    let parsed_instrs = instrs_lines
+        .split("\n")
+        .take_while(|l| !l.is_empty())
+        .map(parse_instr_line);
+
+    // TODO: The following is a weird way to modify a map
+    for mv in parsed_instrs {
+        let mut to_mv = Vec::new();
+        m.entry(mv.from)
+            .and_modify(|x| {
+                let mut n = mv.n;
+                while n > 0 {
+                    x.pop().map(|popped| to_mv.push(popped));
+                    n -= 1;
+                }
+            })
+            .or_default();
+
+        m.entry(mv.to).and_modify(|x| {
+            let to_mv_ = {
+                to_mv.reverse();
+                &mut to_mv
+            }; // TODO: This is the only difference from part1
+            x.append(to_mv_)
+        });
+    }
+
+    let mut str_vec: Vec<_> = Vec::new();
+
+    for val in m.values() {
+        let last = val.last();
+        last.map(|&c| str_vec.push(c));
+    }
+
+    str_vec.into_iter().collect()
+}
+
 pub fn run() {
     let cwd = env::current_dir().unwrap();
     let input = fs::read_to_string(cwd.join("../input/day05")).expect("Failed to read input");
@@ -107,6 +173,6 @@ pub fn run() {
 
     println!("part 1");
     println!("{}", part1(input_str));
-    // println!("part 2");
-    // println!("{}", part2(input_str));
+    println!("part 2");
+    println!("{}", part2(input_str));
 }
