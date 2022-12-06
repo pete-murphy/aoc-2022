baf00369b2a33ffea51e4e9958936db6bd9dbd70
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2022-12-06 00:32:59 -0500

    Day 6, part 2 (Haskell)

--- hs/src/Day06.hs
+++ hs/src/Day06.hs
@@ -12,20 +12,19 @@ part1 =
     >>> head
     >>> fst
 
+part2 :: String -> Int
+part2 =
+  List.tails
+    >>> map (take 14)
+    >>> zip [14 ..]
+    >>> dropWhile (\(_, xs) -> List.nub xs /= xs)
+    >>> head
+    >>> fst
+
 run :: IO ()
 run = do
   input <- readFile "../input/day06"
-  -- let s1 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
-  --     s2 = "nppdvjthqldpwncqszvftbrmjlhg"
-  --     s3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
-  --     s4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
   putStrLn "part 1"
   print (part1 input)
-
-  -- print (part1 s1)
-  -- print (part1 s2)
-  -- print (part1 s3)
-  -- print (part1 s4)
-
   putStrLn "part 2"
-  -- putStrLn (part2 input)
+  print (part2 input)
